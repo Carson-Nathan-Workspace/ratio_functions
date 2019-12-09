8b3da82aa9aa7996221277e193ec9ab6ad2196e7
nathanlucas12 <58672343+nathanlucas12@users.noreply.github.com>
2019-12-08 18:49:23 -0600

    Add files via upload

---
+++ littlesumthing.py
@@ -0,0 +1,36 @@
+marketsept = open("spseptember2019.txt", "r")
+
+lines = marketsept.readlines()
+
+adj_close = []
+
+for line in lines:
+        print(line)
+        #data = line.split(" ")
+        # for point in data:
+                # point.strip()
+                # print(point)
+
+marketsept.close()
+
+
+def jensenalpha(ret_i, ret_rf, b, ret_m):
+        jalpha = ret_i - (b * (ret_m - ret_rf) + ret_rf)
+        return jalpha
+
+
+def capm(ret_rf, b, er_m):
+        er_i = ret_rf + (b * (er_m - ret_rf))
+        return er_i
+
+
+def alpha_fcn(ret_i, ret_m):
+        alpha = ret_i - ret_m
+        return alpha
+
+
+def beta_fcn(covariance, variance):
+        beta = covariance / variance
+        return beta
+
+
